0f81690167d277f6b2e6cfe1e1596f540af73b62
xblazegmd <santiagopalaciocolina7@gmail.com>
2026-04-10 08:26:20 -0500

    debugging

--- src/routes/patreon/tier/+server.ts
+++ src/routes/patreon/tier/+server.ts
@@ -12,6 +12,7 @@ export async function GET({ url }) {
         .from("members")
         .select("patreon_access_token")
         .eq("account_id", Number(accountID));
+    console.log(data[0]);
 
     if (error) {
         throw httpError(500, `Could not fetch info from database: ${error.message}`);
